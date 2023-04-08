72e2cd2fd8884ee05dfabd8843d33864e42c662c
변현호 <hyeonho0523@gmail.com>
2023-04-09 05:04:15 +0900

    상품 상세 > 수량 조절 컴포넌트 추가

--- src/component/organisms/product/productDetailSummaryPanel.tsx
+++ src/component/organisms/product/productDetailSummaryPanel.tsx
@@ -1,6 +1,7 @@
 import { useStyletron } from 'styletron-react';
 import { DetailThumbnailCarousel } from '@component/organisms/carousel/productDetail/detailThumbnailCarousel';
 import { SummaryInfo } from '@component/molecules/product/detail/summaryInfo';
+import { ProductQuantity } from '@component/molecules/product/productQuantity';
 
 const thumbnailDummy: string[] = [
   '/dummy/image/ice_americano.png',
@@ -24,16 +25,36 @@ const ProductDetailSummaryPanel = ({ id }: ProductDetailSummaryPanelProps) => {
       })}
     >
       <DetailThumbnailCarousel thumbnails={thumbnailDummy} />
-      <SummaryInfo
-        title="아이스 아메리카노"
-        price={100000}
-        sellerName="변현호"
-        sellerId="hyeonho"
-      />
-      {/* 수량 */}
-      {/* 옵션 */}
-      {/* 장바구니 */}
-      {/* 바로구매 */}
+      <div
+        className={css({
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        })}
+      >
+        <SummaryInfo
+          title="아이스 아메리카노"
+          price={100000}
+          sellerName="변현호"
+          sellerId="hyeonho"
+        />
+        <section
+          className={css({
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'end',
+            columnGap: '24px',
+            marginTop: '24px',
+          })}
+        >
+          <ProductQuantity />
+          {/* 수량 */}
+          {/* 옵션 */}
+          {/* 장바구니 */}
+          {/* 바로구매 */}
+          {/*  총 상품 금액 */}
+        </section>
+      </div>
     </article>
   );
 };
