031b531d165680c30e2a5423b71dad2f61ab556d
변현호 <hyeonho0523@gmail.com>
2023-04-27 20:24:57 +0900

    viewChanger 컴포넌트에 전역 상태 연결 및 주석 제거

--- src/store/product/viewChanger/viewInfoState.ts
+++ src/store/product/viewChanger/viewInfoState.ts
@@ -1,9 +1,12 @@
 import { atom } from 'recoil';
 import { VIEW_CHANGER_STATES } from '@store/product/viewChanger/viewChanger.states';
 
-const viewInfoState = atom<string>({
+type ViewInfoType = 'grid' | 'list';
+
+const viewInfoState = atom<ViewInfoType>({
   key: VIEW_CHANGER_STATES.viewChanger,
   default: 'grid',
 });
 
+export type { ViewInfoType };
 export { viewInfoState };
